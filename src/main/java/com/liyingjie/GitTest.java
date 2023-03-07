476fa18ad5128d38e9608e0fc6e2c068f17c3e7f
yingjie <yingjie@item.com>
2023-03-07 16:44:11 +0800

    tow commit

---
+++ src/main/java/com/liyingjie/GitTest.java
@@ -0,0 +1,14 @@
+package com.liyingjie;
+
+/**
+ * @author shkstart
+ * @create 2023-03-07-16:38
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git1");
+    }
+
+}
